e2793858b8e48fd0d65993e428a68048ba73668b
Mohammed Abdul Rizwan <50938616+rizwan100@users.noreply.github.com>
2025-08-19 19:43:44 -0500

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,10 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-   eslint: {
+  module.exports = {
+  eslint: {
     ignoreDuringBuilds: true,
   },
-  module.exports = {
   output: 'standalone',
 };
 
